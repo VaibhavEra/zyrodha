1e8eebd3aaeadaf6e40405870672347477282ef0
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-04-16 20:17:38 +0000

    feat: UI, add UI components to homepage

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -1,5 +1,42 @@
 const Awards = () => {
-  return <div>Awards</div>;
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5">
+          <img src="largestBroker.svg" alt="" />
+        </div>
+        <div className="col-6 mt-3">
+          <h1>Largest stock broker in India</h1>
+          <p className="mb-5">
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order volumes in India daily by trading and investing in:
+          </p>
+
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li>Future and Options</li>
+                <li>Commodity derivaties</li>
+                <li>Currency derivaties</li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Stocks & IPOs</li>
+                <li>Direct mutual funds</li>
+                <li>Bonds and Gold</li>
+              </ul>
+            </div>
+          </div>
+          <img
+            src="pressLogos.png"
+            alt="Press Logos"
+            style={{ widht: "50%" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Awards;
--- frontend/src/landing_page/home/Stats.jsx
+++ frontend/src/landing_page/home/Stats.jsx
@@ -1,5 +1,47 @@
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="fs-2 mb-5">Trust with Confidence</h1>
+          <h2 className="fs-4">Customer-First always</h2>
+          <p className="text-muted">
+            That's why 1.3+ crore customers trust Zerodha with 3.5+ lakh crores
+            worth of equity investments.
+          </p>
+          <h2 className="fs-4">No spam or gimmicks</h2>
+          <p className="text-muted">
+            no gimmicks, spams, "gamification", or annoying push notifications.
+            High quality apps that you use at your pace, the way you like.
+          </p>
+          <h2 className="fs-4">The Zerodha Universe</h2>
+          <p className="text-muted">
+            Not just an app, but a whole ecosystem. Our investments in 30+
+            fintech startups offer you tailored services specific to your needs.
+          </p>
+          <h2 className="fs-4">Do better with money</h2>
+          <p className="text-muted">
+            With initiatives like Nudge and Kill switch, we don't just
+            facilitate transactions, but acitvely help you do better with your
+            money.
+          </p>
+        </div>
+        <div className="col-6">
+          <img src="ecosystem.png" alt="ecosystem" style={{ width: "100%" }} />
+          <div className="text-center">
+            <a href="" className="mx-5" style={{ textDecoration: "none" }}>
+              Explore our products{" "}
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+            <a href="" style={{ textDecoration: "none" }}>
+              Try Kite Demo{" "}
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
