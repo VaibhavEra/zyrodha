80c6615a6f2b5cd7ebe6ee31584d3055a8ca5585
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-04-21 12:55:15 +0000

    feat: add charts using charts.js

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 // import { holdings } from "../data/data";
 import axios from "axios";
+import { VerticalGraph } from "./VerticalGraph";
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
@@ -10,6 +11,20 @@ const Holdings = () => {
       setAllHoldings(res.data);
     });
   }, []);
+
+  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = allHoldings.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allHoldings.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
 
   return (
     <>
@@ -74,6 +89,7 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   );
 };
