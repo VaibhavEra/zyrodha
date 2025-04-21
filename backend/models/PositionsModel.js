dfa955c2f163f0b191ead1a2e9f349ec5dff7047
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-04-21 07:45:29 +0000

    feat: fetch holdings data from db via api endpoint

--- backend/models/PositionsModel.js
+++ backend/models/PositionsModel.js
@@ -1,5 +1,5 @@
 const { model } = require("mongoose");
-const { PositionsModel } = require("../schemas/PositionsModel");
-const PositionsModel = new model("position", PositionsModel);
+const { PositionsSchema } = require("../schemas/PositionsSchema");
+const PositionsModel = new model("position", PositionsSchema);
 
 module.exports = { PositionsModel };
